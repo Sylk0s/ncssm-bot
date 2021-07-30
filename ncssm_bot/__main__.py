be65b876a1f95f753d7fde61717c1f09d41b8b18
Sylk0s <sylkos49@gmail.com>
2021-07-30 14:28:08 -0400

    sketchy reaction roles

--- ncssm_bot/__main__.py
+++ ncssm_bot/__main__.py
@@ -2,6 +2,7 @@ import discord
 import ncssm_bot
 
 intents = discord.Intents.default()
+intents.members = True
 
 bot = ncssm_bot.NcssmBot(intents=intents)
 bot.run(bot.token)
--- ncssm_bot/ncssm_bot.py
+++ ncssm_bot/ncssm_bot.py
@@ -8,6 +8,35 @@ class NcssmBot(discord.Client):
         self.configs = conf.BotConfigs()
         self.token = self.configs.getToken()
 
+        self.role_message_id = 870716858616135680
+        self.emoji_to_role = {
+            discord.PartialEmoji(name='🅰️'): 870719903664050176, 
+            discord.PartialEmoji(name='🅱️'): 870719952527716362,
+        }
+
+    async def on_raw_reaction_add(self, theReaction: discord.RawReactionActionEvent):
+        if theReaction.message_id != self.role_message_id:
+            return
+        try:
+            role_id = self.emoji_to_role[theReaction.emoji]  
+        except KeyError:
+            return
+        guild = self.get_guild(theReaction.guild_id)
+        role = guild.get_role(role_id)
+        await theReaction.member.add_roles(role)
+
+    async def on_raw_reaction_remove(self, reaction: discord.RawReactionActionEvent):
+        if reaction.message_id != self.role_message_id:
+            return
+        try:
+            role_id = self.emoji_to_role[reaction.emoji]  
+        except KeyError:
+            return
+        guild = self.get_guild(reaction.guild_id)
+        role = guild.get_role(role_id)
+        member = guild.get_member(reaction.user_id)
+        await member.remove_roles(role)
+
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
